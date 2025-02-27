ac3ad592294c64fd3e9ae1dba0bf4bc369528eb6
Jirch <jirchgot@gmail.com>
2025-02-27 08:14:59 -0700

    Created Circle Class

--- Lab_Creating_Exceptions/Circle.cs
+++ Lab_Creating_Exceptions/Circle.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Lab_Creating_Exceptions
 {
-    internal class Circle
+    public class Circle
     {
+        private double radius;
+        private const double MAX_RADIUS = 20;
+        
+        //constructor
+        public Cricle(double newRadius)
+        {
+            //If the newRadius is within the valid range, the constructor sets the radius to be the newRadius value.  
+            if (newRadius > 0 && newRadius <= MAX_RADIUS)
+            {
+                radius = newRadius;
+            }
+            //If the newRadius is negative, the constructor throws the exception InvalidRadiusException passing to it one argument: newRadius. 
+            else if (newRadius < 0)
+            {
+                throw new InvalidRadiusException(newRadius);
+            }
+            //If the newRadius is greater than MAX_RADIUS, the constructor throws the exception InvalidRadiusException passing to it two arguments: newRadius and MAX_RADIUS.
+            else
+            {
+                throw new InvalidRadiusException(newRadius, MAX_RADIUS);
+            }
+        }
+        public string ToString()
+        {
+            return $"Circle[radius={radius}]";
+        }
     }
-}
