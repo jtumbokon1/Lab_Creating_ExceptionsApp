4330e155cb2c0fc1a9c20452c92c40f5640dca58
Jirch <jirchgot@gmail.com>
2025-02-27 08:44:11 -0700

    Added Comments

--- Lab_Creating_Exceptions/Program.cs
+++ Lab_Creating_Exceptions/Program.cs
@@ -1,4 +1,17 @@
-﻿namespace Lab_Creating_Exceptions
+﻿/*
+ * CPRG-211-C Lab Interfaces and Abstract Classes
+ * Author: Jirch Tumbokon
+ * When: Winter 2025
+ */
+
+/*
+ * This program demonstrates creating and using custom exceptions
+ * by creating a custom exception class called InvalidRadiusException
+ * and using it in the Circle class to throw exceptions when an invalid
+ * input is given for the radius of a circle.
+ */
+
+namespace Lab_Creating_Exceptions
 {
     internal class Program
     {
@@ -6,17 +19,20 @@
         {
             static void MakeCircle(double radius)
             {
+                //try to create a Circle object with the given radius
                 try
                 {
                     Circle circle = new Circle(radius);
                     Console.WriteLine(circle.ToString());
                 }
+                //catch InvalidRadiusException if radius is invalid and display its message
                 catch (InvalidRadiusException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
 
+            //call MakeCircle with different radii
             MakeCircle(25);
             MakeCircle(-20);
             MakeCircle(20);
